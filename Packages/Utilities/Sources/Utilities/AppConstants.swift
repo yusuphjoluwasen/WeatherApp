2d8fe7db5d7c1d2f810dd6ac9467c9b9e327603b
Oluwaseun Jimoh <j.oluwaseun@verifyme.ng>
2024-07-11 01:00:16 +0100

    added gitignore

--- Packages/Utilities/Sources/Utilities/AppConstants.swift
+++ Packages/Utilities/Sources/Utilities/AppConstants.swift
@@ -13,6 +13,8 @@ public enum Constants {
     public static let weatherdetails = "Weather Details"
     public static let loadhistoricalforecasts = "Load Historical Forecasts"
     public static let nosearches = "No Searches Available"
+    public static let error = "Error"
+    public static let close = "Close"
     
 } 
 
